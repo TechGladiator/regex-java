07b6e2e1d316fd72ef58f668ee95ce08a68304ee
John Stratton <jstrat0407@gmail.com>
2017-12-05 02:52:07 +0000

    changed zipcode to static data

--- Reggie.java
+++ Reggie.java
@@ -6,7 +6,7 @@ class Reggie {
   public static void main(String[] args) {
     Console console = System.console();
     // Your amazing code below
-    String zipCode = console.readLine("Enter your zipcode:  ");
+    String zipCode = "90210";
     if (zipCode.matches("^\\d{5}(-\\d{4})?$")) {
       System.out.printf("%s is a valid zip code%n", zipCode);
     } else {
