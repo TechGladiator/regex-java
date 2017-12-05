7efb9cd41c66818ee245dc01e885e270ec463ce1
John Stratton <jstrat0407@gmail.com>
2017-12-05 02:51:24 +0000

    added geographic segment regex check

--- Reggie.java
+++ Reggie.java
@@ -7,7 +7,7 @@ class Reggie {
     Console console = System.console();
     // Your amazing code below
     String zipCode = console.readLine("Enter your zipcode:  ");
-    if (zipCode.matches("\\d{5}")) {
+    if (zipCode.matches("^\\d{5}(-\\d{4})?$")) {
       System.out.printf("%s is a valid zip code%n", zipCode);
     } else {
       System.out.printf("%s is NOT a valid zip code%n", zipCode);
