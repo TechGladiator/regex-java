91ecf6514d09538608591d582d2b0644a98cf714
John Stratton <jstrat0407@gmail.com>
2017-12-05 02:56:56 +0000

    added shushy words regex

--- Reggie.java
+++ Reggie.java
@@ -12,10 +12,18 @@ class Reggie {
     } else {
       System.out.printf("%s is NOT a valid zip code%n", zipCode);
     }
-    String skills = "JavaScript, HTML, CSS, and Java";
+    String skills = "JavaScript, HTML, CSS and Java";
     for (String skill : skills.split("\\W+(and\\W+)?")) {
       System.out.printf("Skill:  %s %n",
                         skill);
+    }
+    String script = "Procrastination is surely not the destination, should we talk about shiny things?";
+    Pattern pattern = Pattern.compile("(\\w*(sh|ti|su)\\w*)");
+    Matcher matcher = pattern.matcher(script);
+    while (matcher.find()) {
+      System.out.printf("%s is a shushy word because of %s.  %n",
+                        matcher.group(1),
+                        matcher.group(2));
     }
   }
 }
