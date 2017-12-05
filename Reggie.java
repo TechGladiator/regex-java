85ad395902adbf54043bd80c52c1fd07b77026bf
John Stratton <jstrat0407@gmail.com>
2017-12-05 02:49:10 +0000

    added zipcode validation via regex

--- Reggie.java
+++ Reggie.java
@@ -6,5 +6,11 @@ class Reggie {
   public static void main(String[] args) {
     Console console = System.console();
     // Your amazing code below
+    String zipCode = console.readLine("Enter your zipcode:  ");
+    if (zipCode.matches("\\d{5}")) {
+      System.out.printf("%s is a valid zip code%n", zipCode);
+    } else {
+      System.out.printf("%s is NOT a valid zip code%n", zipCode);
+    }
   }
 }
